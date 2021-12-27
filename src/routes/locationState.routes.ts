1b766f27fef4bac9c707d8f4111ed5d9fce688e6
keselme90 <keselme90@gmail.com>
2021-12-27 08:04:56 +0200

    Create locationState.routes.ts

---
+++ src/routes/locationState.routes.ts
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import { LocationState, Error } from '../models';
+
+const locationStateRouter = Router();
+
+locationStateRouter.get('/', (request, response) => {
+    locationState.find()
+    .then((data:any) => {
+        response.json(data)
+    })
+    .catch((e:any)=>{
+        console.log(e);
+        response.send(`failed with error ${e}`);
+    });
+});
+
+locationStateRouter.post('/', (request, response) => {
+    const locationState = new LocationState({
+        locationState: request.body.locationState,
+        date: request.body.date,
+    });
+    locationState.save()
+    .then((data:any) => response.json(data))
+    .catch ((e:any) => {
+        const error = new Error({
+            errorMessage: JSON.stringify(`failed to add location state with error: ${e}`),
+            time: new Date()
+        });
+        error.save()
+        .then((data:any) => {
+            response.send(`Error logged successfully for journey with id of ${request.body.journeyId}`);
+        })
+        .catch((e:any) => {
+            console.log(e);
+            response.send(`failed with error ${JSON.stringify(e)}`)
+        });
+    })
+});
+
+export default bluetoothConnectionRouter;
