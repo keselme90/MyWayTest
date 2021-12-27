92fa89e1838a7452a1e3db8a04bfab963ea66552
keselme90 <keselme90@gmail.com>
2021-12-27 08:01:35 +0200

    Create bluetoothConnection.route.ts

---
+++ src/routes/bluetoothConnection.route.ts
@@ -0,0 +1,42 @@
+import { Router } from 'express';
+import { BluetoothConnection, Error } from '../models';
+
+const bluetoothConnectionRouter = Router();
+
+bluetoothConnectionRouter.get('/', (request, response) => {
+    BluetoothConnection.find()
+    .then((data:any) => {
+        response.json(data)
+    })
+    .catch((e:any)=>{
+        console.log(e);
+        response.send(`failed with error ${e}`);
+    });
+});
+
+bluetoothConnectionRouter.post('/', (request, response) => {
+    const bluetoothConnection = new BluetoothConnection({
+        connectionState: request.body.connectionState,
+        btUUID: request.body.btUUID,
+        btName: request.body.btName || '0',
+        date: request.body.date || '0',
+    });
+    bluetoothConnection.save()
+    .then((data:any) => response.json(data))
+    .catch ((e:any) => {
+        const error = new Error({
+            errorMessage: JSON.stringify(`failed to add bluetoothConnection with uuid ${request.body.btUUID} with error: ${e}`),
+            time: new Date()
+        });
+        error.save()
+        .then((data:any) => {
+            response.send(`Error logged successfully for journey with id of ${request.body.journeyId}`);
+        })
+        .catch((e:any) => {
+            console.log(e);
+            response.send(`failed with error ${JSON.stringify(e)}`)
+        });
+    })
+});
+
+export default bluetoothConnectionRouter;
