9691ce8f67def994b331476cf590f981f4e80b7f
keselme90 <keselme90@gmail.com>
2022-05-04 09:24:29 +0300

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import journeyRouter from './journey.routes';
 import errorRouter from './error.routes';
 import locationStateRouter from './locationState.routes';
 import bluetoothConnectionRouter from './bluetoothConnection.routes';
+import bluetoothPairingRouter from './bluetoothPairing.routes
 import reqeustRouter from './request.routes';
 const routes = Router();
 
@@ -13,6 +14,7 @@ routes.get('/', (request, response) => {
 routes.use('/journeys', journeyRouter);
 routes.use('/location-states', locationStateRouter);
 routes.use('/bluetooth-connections', bluetoothConnectionRouter);
+routes.use('/bluetooth-pairing', bluetoothPairingRouter);
 routes.use('/errors', errorRouter);
 routes.use('requests', reqeustRouter);
 
