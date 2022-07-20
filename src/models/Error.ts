a7991a307ad37d430386c9768aff781b51c400f7
keselme90 <keselme90@gmail.com>
2022-07-20 10:59:08 +0300

    Update Error.ts

--- src/models/Error.ts
+++ src/models/Error.ts
@@ -4,7 +4,43 @@ const ErrorSchema = new mongoose.Schema({
     journeyId: String,
     sdkVehicleId: String,
     errorMessage: String,
-    time: Date
+    time: Date,
+    appVersion: {
+        type: String,
+        default: '0.0'
+    },
+    buildVersion: {
+        type: String,
+        default: '0.0'
+    },
+    manufacturer: {
+        type: String,
+        default: 'Unknown'
+    },
+    deviceBrand: {
+        type: String,
+        default: 'Unknown'
+    },
+    deviceId: {
+        type: String,
+        default: 'Unknown'
+    },
+    deviceModel: {
+        type: String,
+        default: 'Unknown'
+    },
+    deviceName: {
+        type: String,
+        default: 'Unknown'
+    },
+    osVersion: {
+        type: String,
+        default: '0.0'
+    },
+    batteryLevel: {
+        type: Number,
+        default: -1
+    },
 });
 
 export default mongoose.model('Error', ErrorSchema);
