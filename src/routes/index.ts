c93daafb88f147d5f5467eb4ef369414305c8a72
keselme90 <keselme90@gmail.com>
2022-08-04 08:49:00 +0300

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,21 +1,24 @@
 import { Router } from 'express';
-import journeyRouter from './journey.routes';
 import errorRouter from './error.routes';
-import locationStateRouter from './locationState.routes';
-import bluetoothConnectionRouter from './bluetoothConnection.routes';
-import bluetoothPairingRouter from './bluetoothPairing.routes'
 import reqeustRouter from './request.routes';
+import journeyRouter from './journey.routes';
+import easterEggRouter from './easterEgg.routes';
+import locationStateRouter from './locationState.routes';
+import bluetoothPairingRouter from './bluetoothPairing.routes'
+import bluetoothConnectionRouter from './bluetoothConnection.routes';
+
 const routes = Router();
 
 routes.get('/', (request, response) => {
     response.send('We are on home');
 });
 
-routes.use('/journeys', journeyRouter);
-routes.use('/location-states', locationStateRouter);
-routes.use('/bluetooth-connections', bluetoothConnectionRouter);
-routes.use('/bluetooth-pairing', bluetoothPairingRouter);
 routes.use('/errors', errorRouter);
+routes.use('/journeys', journeyRouter);
 routes.use('/requests', reqeustRouter);
+routes.use('/easter', eastereEggRouter);
+routes.use('/location-states', locationStateRouter);
+routes.use('/bluetooth-pairing', bluetoothPairingRouter);
+routes.use('/bluetooth-connections', bluetoothConnectionRouter);
 
 export default routes;
