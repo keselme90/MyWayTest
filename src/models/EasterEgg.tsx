0e98833adf63c9f56337c924adea7397b49c96a2
keselme90 <keselme90@gmail.com>
2022-08-04 08:36:07 +0300

    Create EasterEgg.tsx

---
+++ src/models/EasterEgg.tsx
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose';
+
+const EasterEggSchema =  new mongoose.Schema({
+    timestamp: {
+        type: Date,
+        default: undefined
+    },
+    env: {
+        type: String,
+        default: undefined,
+    },
+    sdkDriverId: {
+        type: String,
+        default: undefined
+    },
+    sdkPolicyId: {
+        type: String,
+        default: undefined
+    },
+    sdkVehicleId: {
+        type: String,
+        default: undefined
+    },
+    hafakaVehicleId: {
+        type: String,
+        default: undefined
+    },
+    sdkVersion: {
+        type: String,
+        default: undefined
+    },
+    isLoggedIn: {
+      type: Boolean,
+      default: false
+    },
+    isServiceReady: {
+        type: Boolean,
+        default: false
+    },
+    manufacturer: {
+        type: String,
+        default: undefined
+    },
+    deviceName: {
+        type: Boolean,
+        default: false
+    },
+    model: {
+        type: String,
+        default: undefined
+    },
+    osName: {
+        type: String,
+        default: undefined
+    },
+    osVersion: {
+        type: String,
+        default: undefined
+    }
+});
+
+
+
+export default mongoose.model('EasterEgg', EasterEggSchema);
